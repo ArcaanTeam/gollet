ae03dc7f015e239d533b4f988a7b88e2d8a5c9ba
Javad Mohammadi <javadsemail@gmail.com>
2025-07-28 10:36:20 +0330

    ✨ Setting up gin app and wiring user routes and controllers

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"gollet/config"
+	"gollet/controllers"
 	"gollet/db"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadConfig()
 	db.InitDB()
 	db.Migrate()
+
+	userController := controllers.NewUserController(db.DB)
+
+	r := gin.Default()
+
+	r.POST("/users", userController.CreateUser)
+
+	r.Run(":8080")
 }
