673fde2f56149c455dc2438c0250e36b514ca0e5
Javad Mohammadi <javadsemail@gmail.com>
2025-07-28 16:29:06 +0330

    ✨ Add `/profile` route and wire it up with GetProfile handler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"gollet/config"
 	"gollet/controllers"
 	"gollet/db"
+	"gollet/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +21,11 @@ func main() {
 
 	r.POST("/users", userController.CreateUser)
 	r.POST("/login", authController.Login)
+	protectedRoutes := r.Group("/")
+	protectedRoutes.Use(middlewares.JwtAuthMiddleware())
+	{
+		protectedRoutes.GET("/profile", userController.GetProfile)
+	}
 
 	r.Run(":8080")
 }
