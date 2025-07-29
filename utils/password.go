7e14ee2d0fdfd25016808283b46fabe31920319e
Javad Mohammadi <javadsemail@gmail.com>
2025-07-29 16:57:50 +0330

    ✅ Pass `Empty password` test

--- utils/password.go
+++ utils/password.go
@@ -1,10 +1,15 @@
 package utils
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
+	if len(password) < 6 {
+		return "", errors.New("password must be at least 6 characters long")
+	}
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
