92d89e3dcc67a643c92612908057f3540b888a8c
Javad Mohammadi <javadsemail@gmail.com>
2025-07-29 11:20:35 +0330

    ✨ Add `role` field to user model and disable PasswordHash json visibility

--- models/user.go
+++ models/user.go
@@ -4,9 +4,15 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	RoleAdmin string = "admin"
+	RoleUser  string = "user"
+)
+
 type User struct {
 	gorm.Model
 	Name         string `gorm:"not null" json:"name"`
 	Email        string `gorm:"type:citext;not null;unique" json:"email"`
-	PasswordHash string `gorm:"not null"`
+	Role         string `gorm:"type:varchar(20);not null;default:'user'"`
+	PasswordHash string `gorm:"not null" json:"-"`
 }
