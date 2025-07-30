48ed11d197c85e657ce6e614c40d0e172dd40bbd
Javad Mohammadi <javadsemail@gmail.com>
2025-07-30 17:11:49 +0330

    ✨ Add `user` and `auth` repositories

---
+++ repositories/auth_repository.go
@@ -0,0 +1,28 @@
+package repositories
+
+import (
+	"gollet/models"
+
+	"gorm.io/gorm"
+)
+
+type AuthRepository interface {
+	GetUserByEmail(email string) (*models.User, error)
+}
+
+type authRepository struct {
+	db *gorm.DB
+}
+
+func NewAuthRepository(db *gorm.DB) AuthRepository {
+	return &authRepository{db: db}
+}
+
+func (r *authRepository) GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	if err := r.db.First(&user, "email = ?", email).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
