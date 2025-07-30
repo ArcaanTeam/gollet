d29dcab669f5a863ddb3c2f4ca701fef0ecfaac4
Javad Mohammadi <javadsemail@gmail.com>
2025-07-30 17:19:40 +0330

    🚚 Move database to `config` package

--- main.go
+++ main.go
@@ -3,22 +3,21 @@ package main
 import (
 	"gollet/api/routes"
 	"gollet/config"
-	"gollet/db"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadConfig()
-	db.InitDB()
-	db.Migrate()
+	config.InitDB()
+	config.Migrate()
 
 	engine := gin.Default()
 
-	userRoutes := routes.NewUserRoutes(db.DB, engine)
+	userRoutes := routes.NewUserRoutes(config.DB, engine)
 	userRoutes.Setup()
 
-	authRoutes := routes.NewAuthRoutes(db.DB, engine)
+	authRoutes := routes.NewAuthRoutes(config.DB, engine)
 	authRoutes.Setup()
 
 	engine.Run(":8080")
